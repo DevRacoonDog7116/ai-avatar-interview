d094c92de27c03f2b83e1b6aab425343bd272541
Fernando_O <fernandoolarte2002@gmail.com>
2025-04-25 12:26:03 +0900

    Update Scenario.jsx

--- apps/frontend/src/components/Scenario.jsx
+++ apps/frontend/src/components/Scenario.jsx
@@ -1,24 +1,13 @@
-import { CameraControls, Environment, useTexture } from "@react-three/drei";
+import { CameraControls, Environment } from "@react-three/drei";
 import { useEffect, useRef } from "react";
 import { Avatar } from "./Avatar";
 
-const Ground = () => {
-  const texture = useTexture(
-    "https://cdn.polyhaven.org/textures/jpeg/2k/grass_0002_2k.jpg"
-  );
-
-  return (
-    <mesh rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
-      <planeGeometry args={[1000, 1000]} />
-      <meshStandardMaterial
-        map={texture}
-        map-repeat={[100, 100]}
-        map-wrapS={1000}
-        map-wrapT={1000}
-      />
-    </mesh>
-  );
-};
+const Ground = () => (
+  <mesh rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
+    <planeGeometry args={[1000, 1000]} />
+    <meshStandardMaterial color="#228B22" />
+  </mesh>
+);
 
 export const Scenario = () => {
   const cameraControls = useRef();
