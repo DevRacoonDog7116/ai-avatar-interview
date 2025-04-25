d59f1388acbcfded720b6e0940b922da6b0a34ff
Fernando_O <fernandoolarte2002@gmail.com>
2025-04-25 12:30:26 +0900

    Update Scenario.jsx

--- apps/frontend/src/components/Scenario.jsx
+++ apps/frontend/src/components/Scenario.jsx
@@ -2,12 +2,6 @@ import { CameraControls, Environment } from "@react-three/drei";
 import { useEffect, useRef } from "react";
 import { Avatar } from "./Avatar";
 
-const Ground = () => (
-  <mesh position={[0, -3, 0]} rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
-    <planeGeometry args={[1000, 1000]} />
-    <meshStandardMaterial color="white" />
-  </mesh>
-);
 
 export const Scenario = () => {
   const cameraControls = useRef();
@@ -22,7 +16,6 @@ export const Scenario = () => {
         background
         files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/2k/docklands_02_2k.hdr"
       />
-      <Ground />
       <Avatar />
     </>
   );
