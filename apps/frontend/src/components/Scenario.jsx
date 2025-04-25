dbe60603e6157c0fa8c6246fd1846a21f8b79afd
Fernando_O <fernandoolarte2002@gmail.com>
2025-04-25 12:05:27 +0900

    Update Scenario.jsx

--- apps/frontend/src/components/Scenario.jsx
+++ apps/frontend/src/components/Scenario.jsx
@@ -1,33 +1,16 @@
-import { CameraControls } from "@react-three/drei";
+import { CameraControls, Environment } from "@react-three/drei";
 import { useEffect, useRef } from "react";
 import { Avatar } from "./Avatar";
-import * as THREE from "three";
 
 export const Scenario = () => {
   const cameraControls = useRef();
-
-  // Set the initial camera position and look-at point
   useEffect(() => {
     cameraControls.current.setLookAt(0, 2.2, 5, 0, 1.0, 0, true);
   }, []);
-
   return (
     <>
-      {/* Camera controls */}
       <CameraControls ref={cameraControls} />
-      
-      {/* Background sphere with HDRI */}
-      <mesh>
-        <sphereGeometry args={[500, 60, 60]} />
-        <meshBasicMaterial
-          side={THREE.BackSide} // Ensures it's visible from inside the sphere
-          map={new THREE.TextureLoader().load(
-            "https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/4k/docklands_02_4k.hdr"
-          )}
-        />
-      </mesh>
-      
-      {/* Your Avatar component */}
+      <Environment files="https://blenderartists.org/uploads/default/original/3X/6/c/6c3c595edc8b349b2028b28a2f570e89ea7ae7fd.jpg" background/>
       <Avatar />
     </>
   );
