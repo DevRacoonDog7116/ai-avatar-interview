a10a862264d283fd8175c3f60805275ab7bac2ac
S Future <tiaguitov08@gmail.com>
2025-04-26 11:20:31 +0900

    Update rhubarbLipSync.mjs

--- apps/backend/modules/rhubarbLipSync.mjs
+++ apps/backend/modules/rhubarbLipSync.mjs
@@ -5,12 +5,12 @@ const getPhonemes = async ({ message }) => {
     const time = new Date().getTime();
     console.log(`Starting conversion for message ${message}`);
     await execCommand(
-      { command: `ffmpeg -y -i audios/message_${message}.mp3 audios/message_${message}.wav` }
+      { command: `ffmpeg -y -i tmp/message_${message}.mp3 tmp/message_${message}.wav` }
       // -y to overwrite the file
     );
     console.log(`Conversion done in ${new Date().getTime() - time}ms`);
     await execCommand({
-      command: `./bin/rhubarb -f json -o audios/message_${message}.json audios/message_${message}.wav -r phonetic`,
+      command: `./bin/rhubarb -f json -o tmp/message_${message}.json tmp/message_${message}.wav -r phonetic`,
     });
     // -r phonetic is faster but less accurate
     console.log(`Lip sync done in ${new Date().getTime() - time}ms`);
